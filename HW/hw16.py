4160fd63875a3a9d6dfbbf9b1ea060f6205ec660
natazhigaleva <jigalevan@gmail.com>
2023-11-19 02:21:27 +0500

    Update hw16.py

--- HW/hw16.py
+++ HW/hw16.py
@@ -3,10 +3,13 @@
 # которые отделены пробелами. Возвращаемое значение должно быть типа int. 
 # Применить эту функцию для файла и найти общее кол-во таких чисел.
 
-with open("exampple.txt", 'r') as f:
+with open("example.txt", 'r') as f:
     s = f.read()
 
 def check_isdigit(s):
     s = input().split()
     nums = list(filter(lambda num: num.isdigit(), s))
     print(int(len(nums)))
+
+# Lorem.txt ipsum32 dolor 56 sit amet 1, consec12tetur adipiscing 4 elit. Aenean 25 ultrices.
+# 3
